64c0307e538b3e8489d4a477f0652dd5c1888b1b
chilingirov <vasko988@gmail.com>
2017-09-25 16:13:29 -0700

    Added gif extra

--- js/index.js
+++ js/index.js
@@ -1,13 +1,23 @@
 function randomQuote() {
+
+    $.ajax({
+        url: "http://api.giphy.com/v1/gifs/random?api_key=1SPxjTceVrzEl44goX8NK3LGFljXPDQt&callback=?",
+        dataType: "jsonp",
+        success: function(data) {
+            $(".right-gif").html("<img src=" + data.data.fixed_height_downsampled_url + " >")
+        }
+    })
     $.ajax({
         url: "https://api.forismatic.com/api/1.0/?",
         dataType: "jsonp",
         data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
         success: function(response) {
-            $("#quote").html("<h2 id='random_quote' class='lead text-center'>" +
+            $(".left-quote").html("<h2 id='random_quote' class='lead text-center'>" +
                 response.quoteText + "<br/>&dash; " + response.quoteAuthor + ' &dash;</h2>' + '<a class="twitter-share-button" href="https://twitter.com/intent/tweet?text=' + response.quoteText + '-' + response.quoteAuthor + '-' + '"' + ' >Tweet</a>');
         }
     });
+
+
 }
 
 function randomColor() {
@@ -18,5 +28,8 @@ function randomColor() {
 
 $("#generate").on("click", function() {
     randomQuote();
-    document.querySelector("body").style.backgroundColor = randomColor();
+    var randomcol = randomColor();
+    document.querySelector("body").style.backgroundColor = randomcol;
+    document.querySelector("#quote").style.color = randomcol;
+    document.querySelector("input").style.color = randomcol;
 });
